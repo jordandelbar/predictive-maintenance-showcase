cf598c201ef73fb3afbeb625d1d620c1657baec7
Jordan Delbar <jordandelbar@gmail.com>
2024-11-03 19:55:02 +0100

    feat: Add migrate service

--- ml_facade/threshold_setup.sh
+++ ml_facade/threshold_setup.sh
@@ -1,1 +1,18 @@
-curl -i -X POST http://localhost:4000/v1/threshold -d '{"machine_id": 7, "threshold": 0.02}'
+HEALTH_CHECK_URL="http://localhost:4000/health"
+ROUTE="http://localhost:4000/v1/threshold"
+
+while true; do
+  echo "Checking health at: $HEALTH_CHECK_URL"
+
+  if curl -X GET "$HEALTH_CHECK_URL"; then
+    if curl -i -X POST "$ROUTE" -d '{"machine_id": 7, "threshold": 0.02}'; then
+      break
+    else
+      echo "Failed to send POST request. Retrying in 3 seconds..."
+      sleep 3
+    fi
+  else
+    echo "Service is not healthy, retrying in 3 seconds..."
+    sleep 3
+  fi
+done
